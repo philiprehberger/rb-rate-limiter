08b8cc4518ce1aec4c88704d1ece64fac6787d10
Philip Rehberger <me@philiprehberger.com>
2026-04-15 12:07:51 -0600

    Release v0.7.0

---
+++ lib/philiprehberger/rate_limiter/noop.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+module Philiprehberger
+  module RateLimiter
+    # A limiter that always allows requests. Useful for tests and feature-flagged rollouts.
+    class Noop
+      def allow?(_key = :default, weight: 1)
+        _ = weight
+        true
+      end
+
+      def allow!(_key = :default, weight: 1)
+        _ = weight
+        true
+      end
+
+      def peek(_key = :default)
+        true
+      end
+
+      def remaining(_key = :default)
+        Float::INFINITY
+      end
+
+      def reset(_key = :default)
+        nil
+      end
+
+      def clear
+        nil
+      end
+
+      def info(_key = :default)
+        { remaining: Float::INFINITY, limit: Float::INFINITY, used: 0 }
+      end
+
+      def stats(_key = :default)
+        { allowed: 0, rejected: 0 }
+      end
+
+      def keys
+        []
+      end
+
+      def throttle(_key = :default, weight: 1, &block)
+        _ = weight
+        { allowed: true, value: block.call }
+      end
+
+      def on_reject(&)
+        self
+      end
+    end
+  end
+end
---
+++ spec/philiprehberger/rate_limiter/noop_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe Philiprehberger::RateLimiter::Noop do
+  let(:limiter) { described_class.new }
+
+  it 'always allows requests' do
+    1000.times { expect(limiter.allow?('k')).to be true }
+  end
+
+  it 'never raises from allow!' do
+    expect(limiter.allow!('k')).to be true
+  end
+
+  it 'reports infinite remaining quota' do
+    expect(limiter.remaining('k')).to eq(Float::INFINITY)
+  end
+
+  it 'throttles blocks as allowed' do
+    result = limiter.throttle('k') { 42 }
+    expect(result).to eq(allowed: true, value: 42)
+  end
+
+  it 'returns empty keys list' do
+    limiter.allow?('k')
+    expect(limiter.keys).to eq([])
+  end
+
+  it 'returns a stable info hash' do
+    info = limiter.info('k')
+    expect(info[:limit]).to eq(Float::INFINITY)
+    expect(info[:used]).to eq(0)
+  end
+
+  it 'is reachable via RateLimiter.noop' do
+    expect(Philiprehberger::RateLimiter.noop).to be_a(described_class)
+  end
+end
